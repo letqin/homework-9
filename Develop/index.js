647f69fced7b790e26a9551f6c464a70e36687a5
letqin <86627655+letqin@users.noreply.github.com>
2021-10-18 03:32:56 -0400

    added inquirer question array

--- Develop/index.js
+++ Develop/index.js
@@ -1,22 +1,57 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateMarkdown = (answers) =>
 
 // TODO: Create an array of questions for user input
 
-const questions = (input) => [
-    "What is the name of your project?",
-    "What was your motivation for the project?",
-    "Why did you build this project?",
-    "What problem is this project solving?",
-    "What did you learn?",
-    "What makes your project stand out?",
-    "What challenges did you come across?",
-    "What is the link to the live deployment/ GitHub repository link?"
-];
+inquirer
+    .prompt ([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the name of your project?",
+        },
+        {
+            type: "input",
+            name: "name",
+            message: "What was your motivation for the project?",
+        },
+        {
+            type: "input",
+            name: "name",
+            message: "Why did you build this project?",
+        },
+        {
+            type: "input",
+            name: "name",
+            message: "What problem is this project solving?",
+        },
+        {
+            type: "input",
+            name: "name",
+            message: "What did you learn?",
+        },
+        {
+            type: "input",
+            name: "name",
+            message: "What makes your project stand out?",
+        },
+        {
+            type: "input",
+            name: "name",
+            message: "What challenges did you come across?",
+        },
+        {
+            type: "input",
+            name: "name",
+            message: "What is the link to the live deployment/ GitHub repository link?",
+        },   
+]);
 
 // TODO: Create a function to write README file
-
+fs.writeFile("README.MD", )
 // Display above questions in the console
+
 // Wait for user input via the command line
 
 
